fa7f2ed77ec2a0d1fc188e9aa41a151a4914d370
gutudaniel <daniel.gutu@gmail.com>
2017-07-22 15:29:50 -0700

    quick_sort.py
    quicksort

---
+++ quick_sort.py
@@ -0,0 +1,10 @@
+def quick_sort(l):
+    if len(l) == 0:
+        return l
+    pivot = l[0]
+    pivots = [x for x in l if x == pivot]
+    smaller = quick_sort([x for x in l if x < pivot])
+    larger = quick_sort([x for x in l if x > pivot])
+    return smaller + pivots + larger
+array = [12, 4, 5, 6, 7, 3, 1, 15]
+print(quick_sort(array))
