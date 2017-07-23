f459adb2af73a3b32e036637ca8c59856ced1ff5
gutudaniel <daniel.gutu@gmail.com>
2017-07-22 17:54:43 -0700

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -6,5 +6,5 @@ def quick_sort(l):
     smaller = quick_sort([x for x in l if x < pivot])
     larger = quick_sort([x for x in l if x > pivot])
     return smaller + pivots + larger
-array = [12, 4, 5, 6, 7, 3, 1, 15, 17]
+array = [12, 4, 5, 6, 7, 3, 1, 15, 17, 19]
 print(quick_sort(array))
